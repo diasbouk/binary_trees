704f236485607ea287dc4e240dde6a5f18846527
diasbouk <boukdsaid@gmail.com>
2024-06-07 00:07:07 +0100

    betty all good

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -14,9 +14,8 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 
 	bt = (binary_tree_t *)malloc(sizeof(binary_tree_t));
 	if (bt == NULL)
-	{
 		return (NULL);
-	}
+
 	bt->parent = parent;
 	bt->n = value;
 	bt->left = NULL;
