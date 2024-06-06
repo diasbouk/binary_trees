01ad711353c91ceb035b853e789137103d9e542b
diasbouk <boukdsaid@gmail.com>
2024-06-07 00:21:35 +0100

    test

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - Creates a new binary tree
+ * @parent: Parent of our binary tree
+ * @value: Value of the root
+ * Return: Pointer to root or null
+ */
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *child;
+	child = binary_tree_node(parent, value);
+
+	if (child == NULL)
+		return (NULL);
+	child->parent = parent;
+	child->n value;
+	parent->left = child;
+
+	return (child);
+}
