46b31a93cee1a750e36e43a4a9c5e98d45b8b4a0
diasbouk <diasbouk>
2024-07-31 18:19:08 +0100

    Task 1 done and good

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,20 +12,15 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	binary_tree_t *new_node;
 
 	if (parent == NULL)
-	{
 		return (NULL);
-	}
-
-	new_node = binary_tree_node(parent, value);
-	if (new_node == NULL)
+	if (parent->left)
 	{
-		return (NULL);
-	}
-	if (parent->left != NULL)
-	{
-		new_node->left = parent->left;
+		new_node = binary_tree_node(parent, value);
 		parent->left->parent = new_node;
+		new_node->left = parent->left;
+		parent->left = new_node;
+		return (new_node);
 	}
-	parent->left = new_node;
-	return (new_node);
+	parent->left = binary_tree_node(parent, value);
+	return (parent->left);
 }
