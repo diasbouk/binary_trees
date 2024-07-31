ca6ab1cc9f77a4cf5b0575df8b1ff15db4c3b3ae
diasbouk <diasbouk>
2024-07-31 18:35:25 +0100

    all done

--- binary_trees.h
+++ binary_trees.h
@@ -32,8 +32,9 @@ typedef struct binary_tree_s binary_tree_t;
 
 
 
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 void binary_tree_print(const binary_tree_t *parent);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 
 #endif 
