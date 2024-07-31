2597d497e8cbe9e13d8c3115b9cfd5d855c1158b
diasbouk <diasbouk>
2024-07-31 18:25:51 +0100

    Task 2 done and good

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_insert_right - Inserts a new node at
+* the right of of parent
+* @parent: Node to inster as its right
+* @value: Value to hold in the new node
+* Return: Pointer to the node or NULL if it fails
+*/
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
+	if (!parent)
+		return (NULL);
+	if (parent->right)
+	{
+		new_node = binary_tree_node(parent, value);
+		parent->right->parent = new_node;
+		new_node->right = parent->right;
+		parent->right = new_node;
+		return (new_node);
+	}
+	parent->right = binary_tree_node(parent, value);
+	return (parent->right);
+
+}
