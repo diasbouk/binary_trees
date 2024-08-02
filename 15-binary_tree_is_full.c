921cdbacaa45c43f88c50625739083fa01341b82
diasbouk <boukdsaid@gmail.com>
2024-08-02 03:38:53 +0100

    Task 15 done , Checking ..

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -11,5 +11,7 @@ int binary_tree_is_full(const binary_tree_t *tree)
 		return (0);
 	if (!(tree->left && tree->right) && (tree->left || tree->right))
 		return (0);
+	binary_tree_is_full(tree->left);
+	binary_tree_is_full(tree->right);
 	return (1);
 }
