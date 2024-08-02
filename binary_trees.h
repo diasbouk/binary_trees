6bab90533bbff8263fc9fecc72d2b0fe035013ee
diasbouk <diasbouk>
2024-08-02 02:42:18 +0100

    Task 10 done , waiting for the checker response

--- binary_trees.h
+++ binary_trees.h
@@ -54,4 +54,6 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 /* Get the height of the binary tree */
 size_t binary_tree_height(const binary_tree_t *tree);
+/* Measures the depth of a node in a binary tree */
+size_t binary_tree_depth(const binary_tree_t *tree);
 #endif 
