e219b8a6dbc184ac74bff13c60b8b9cda963bdf0
diasbouk <boukdsaid@gmail.com>
2024-08-02 19:50:52 +0100

    Task 16 done.

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,6 +1,52 @@
 #include "binary_trees.h"
 
 /**
+	* binary_tree_height - Counts tree height
+	* @tree: Tree to check height
+	* Return: Height of the b_tree
+*/
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	int leftDepth;
+	int rightDepth;
+
+	if (!tree)
+		return (0);
+	if (tree->left)
+		leftDepth = 1 + binary_tree_height(tree->left);
+	else
+		leftDepth = binary_tree_height(tree->left);
+	if (tree->right)
+		rightDepth = 1 + binary_tree_height(tree->right);
+	else
+		rightDepth = binary_tree_height(tree->right);
+	if (leftDepth > rightDepth)
+		return (leftDepth);
+	return (rightDepth);
+}
+
+/**
+	* check_perfection - Checks if its a prefect b_tree
+	* @tree: Pointer to the root node
+	* @level: Level of the node
+	* @depth: Depth of the tree
+	* Return: 1 if it is , 0 otherwise
+*/
+
+int check_perfection(const binary_tree_t *tree, size_t level, size_t depth)
+{
+	if (tree == NULL)
+		return (1);
+	if (tree->left == NULL && tree->right == NULL)
+		return (level == depth);
+	if (tree->left == NULL || tree->right == NULL)
+		return (0);
+
+	return (check_perfection(tree->left, level + 1, depth) &&
+	check_perfection(tree->right, level + 1, depth));
+}
+
+/**
 	* binary_tree_is_perfect - Checks if its a prefect b_tree
 	* @tree: Pointer to the root node
 	* Return: 1 if it is , 0 otherwise
@@ -8,16 +54,11 @@
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int left;
-	int right;
+	size_t depth;
 
-	if (!tree)
+	if (tree == NULL)
 		return (0);
-	if (!(tree->left && tree->right) && (tree->left || tree->right))
-		return (0);
-	if (!tree->left && !tree->right)
-		return (1);
-	left = binary_tree_is_perfect(tree->left);
-	right = binary_tree_is_perfect(tree->right);
-	return (left && right);
+
+	depth = binary_tree_height(tree);
+	return (check_perfection(tree, 0, depth));
 }
--- binary_trees.h
+++ binary_trees.h
@@ -42,7 +42,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 /* Deleting an entire binary_tree */
 void binary_tree_delete(binary_tree_t *tree);
-/* Checks for a node if its a lead */
+/* Checks for a node if its a leaf */
 int binary_tree_is_leaf(const binary_tree_t *node);
 /* Checks if a node is root */
 int binary_tree_is_root(const binary_tree_t *node);
